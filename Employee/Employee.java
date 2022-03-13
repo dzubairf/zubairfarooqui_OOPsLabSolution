1f002ea3c8dc8936405ec70f50819623f0e19970
dzubairf <51185812+dzubairf@users.noreply.github.com>
2022-03-13 13:18:57 +0530

    Removing unused code

--- Employee/Employee.java
+++ Employee/Employee.java
@@ -1,19 +1,11 @@
 package Employee;
 
 public class Employee {
-    private String firstName;
-    private String lastName;
+    String firstName;
+    String lastName;
     public Employee(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Dear " + firstName + " " + lastName +  " your generated credentials are as follows");
     }
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-
-
 }
